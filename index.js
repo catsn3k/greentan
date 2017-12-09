cdbd28f4c5eb7724538d6f8094381df2a8f7ed29
Glitch (greentanbot) <none>
2017-12-09 21:26:27 +0000

    👢🌇 Checkpoint index.js:586861

--- index.js
+++ index.js
@@ -23,38 +23,38 @@ bot.on('message', msg => {
 
   if (msg.content.startsWith(prefix + 'role')) {
     if (msg.member.roles.some(r=>["Owner", "Admin", "Mod", "Dev", "Server Host", "Oldfag", "Newfag"].includes(r.name)) ) {
-
-
-    // Get args
-    let args = msg.content.split(" ");
-
-    if (args.length < 2 || args[1] == '-help') {
-      msg.reply('These are the roles you\'re allowed to join: \n'+
-        allowedString +
-        '\nuse ".role `<role_name>` to join a role');
-    }
-
-    // Get the role
-    let role = msg.guild.roles.find("name", args[1].toLowerCase());
-
-    // Checks for errors after args
-    if (!role || role === null) {
-      msg.reply('Could not find a role by that name.');
-    }
-
-    // Checks for errors after args
-    else if (allowedRoles.indexOf(role.name) === -1) {
-      msg.reply('Doesn\'t look like you\'re allowed to join that group. \nFor a list of allowed roles type `.role -help`');
-    }
       
-    msg.member.addRole(role);
-    msg.reply('You\'ve been added to: ' + role.name + ' Welcome to the army soldier!');
+      // Get args
+      let args = msg.content.split(" ");
 
-    } else {
-      msg.reply("it looks like you don't have the right to use that command");
+      // Assumes if the args length is less than two or is replaced with '-help' then do
+      if (args.length < 2 || args[1] == '-help') {
+        msg.reply('These are the roles you\'re allowed to join: \n'+
+          allowedString +
+          '\nuse ".role `<role_name>` to join a role');
+      }
+
+      // let value for roles from args
+      let role = msg.guild.roles.find("name", args[1].toLowerCase());
+
+      // Checks for errors after args
+      if (!role || role === null) {
+        msg.reply('could not find a role by that name.');
+        
+      } else if (allowedRoles.indexOf(role.name) === -1) {
+        msg.reply('doesn\'t look like you\'re allowed to join that group. \nFor a list of allowed roles type `.role -help`');
+      } else {
+        return
+      }
+
+      msg.member.addRole(role);
+      msg.reply('You\'ve been added to: ' + role.name + ' Welcome to the army soldier!');
+
+      } else {
+        msg.reply("it looks like you don't have the right to use that command");
+      }
     }
-  }
-})
+});
 
 bot.on("guildMemberAdd", member => {
     console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
