592cb22563be7336795f34e78d002d2cbc660156
Glitch (greentanbot) <none>
2017-12-09 20:14:20 +0000

    🔪📷 Checkpoint README.md:586861 server.js:586861 main.js:586861 .env:586861 index.js:586861

---
+++ README.md
@@ -0,0 +1,5 @@
+greentanbot is a Discord bot created for my Discord server: https://discord.gg/EMMmZJU
+
+Currently using code from:
+https://github.com/messerli90/role-bot
+(With some changes)
---
+++ index.js
@@ -0,0 +1,72 @@
+let Discord = require('discord.js')
+let Client = new Discord.Client()
+let dotenv = require('dotenv');
+
+// Grabs the BOT_TOKEN from .env and stores in on the `process.env`
+dotenv.load()
+
+let allowedRoles = process.env.ALLOWED_ROLES.split(',')
+let botToken = process.env.BOT_TOKEN
+
+// allowed strings
+let allowedString = ''
+allowedRoles.forEach((role) => {
+  allowedString = allowedString.concat('* ' + role + '\n')
+})
+
+Client.on('message', msg => {
+  // Set prefix
+  let prefix = "."
+
+  if (!msg.content.startsWith(prefix)
+    || msg.author.bot
+  ) return
+
+  if (msg.content.startsWith(prefix + 'role')) {
+    if (msg.member.)
+
+    // Get args
+    let args = msg.content.split(" ");
+
+    if (args.length < 2 || args[1] == '-help') {
+      msg.channel.sendMessage('These are the roles you\'re allowed to join: \n'+
+        allowedString +
+        '\nuse ".role `<role_name>` to join a role')
+
+      return
+    }
+
+    // Get the role
+    let role = msg.guild.roles.find("name", args[1].toLowerCase());
+
+    if (!role || role === null) {
+      msg.channel.sendMessage('Could not find a role by that name.')
+      return
+    }
+
+    else if (allowedRoles.indexOf(role.name) === -1) {
+      msg.channel.sendMessage('Doesn\'t look like you\'re allowed to join that group. \nFor a list of allowed roles type `.role -help`')
+      return
+    }
+
+    msg.member.addRole(role).catch(console.error);
+    msg.channel.sendMessage('You\'ve been added to: ' + role.name)
+
+    return
+  }
+})
+
+Client.on("guildMemberAdd", member => {
+    console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
+
+    //member.guild.defaultChannel.sendMessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
+})
+
+Client.on('ready', () => {
+  Client.user.setGame('Ace of Spades 0.54)
+  console.log(`Ready to set roles in ${Client.channels.size} channels on ${Client.guilds.size} servers, for a total of ${Client.users.size} users.`)
+})
+
+Client.on('error', e => { console.error(e) })
+
+Client.login(botToken)
